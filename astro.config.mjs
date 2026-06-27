b86758250c171313f16a9a1af80e80acca971024
Tony <silenttoneenterprises@gmail.com>
2026-06-27 09:19:58 -0500

    metadata SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brewdown.vercel.app',
   output: 'static',
   server: {
     host: true,
   },
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
